539847b800d088812209c3e26d7bb24c07e48c3f
Mohamed-Abdalla-Yassen <mabda885@gmail.com>
2026-02-27 16:20:58 +0200

    setup enviroment and bubble sort back

---
+++ Sorting_algo/src/main/java/com/assignment1/sorting_algo/service/Sort.java
@@ -0,0 +1,5 @@
+package com.assignment1.sorting_algo.service;
+
+public interface Sort {
+    void sort();
+}
---
+++ Sorting_algo/src/test/java/com/assignment1/sorting_algo/SortingAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment1.sorting_algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAlgoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
