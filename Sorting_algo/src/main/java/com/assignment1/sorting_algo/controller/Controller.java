6b7bf6390e71f9fffccfd05e62d599b6d0898fd0
Mohamed-Abdalla-Yassen <mabda885@gmail.com>
2026-02-28 16:45:02 +0200

    merge -back last one was heap

--- Sorting_algo/src/main/java/com/assignment1/sorting_algo/controller/Controller.java
+++ Sorting_algo/src/main/java/com/assignment1/sorting_algo/controller/Controller.java
@@ -14,17 +14,22 @@ public class Controller {
     private final BubbleSort bubble_service;
     private final SelectionSort selection_service;
     private final InsertionSort insertion_service;
-//    private final MergeSort merge_service;
+    private final MergeSort merge_service;
     private final QuickSort quick_service;
+    private final HeapSort heap_service;
     // Constructor injection
     public Controller(BubbleSort bubble_service,
                       SelectionSort selection_service,
                       InsertionSort insertion_service,
-                      QuickSort quick_service) {
+                      QuickSort quick_service,
+                      HeapSort heap_service,
+                      MergeSort merge_service) {
         this.bubble_service = bubble_service;
         this.selection_service = selection_service;
         this.insertion_service = insertion_service;
         this.quick_service = quick_service;
+        this.heap_service = heap_service;
+        this.merge_service = merge_service;
     }
 
     @PostMapping("/bubble_sort")
@@ -45,5 +50,15 @@ public class Controller {
     @PostMapping("/quick_sort")
     public List<Step> quick_sort(@RequestBody RequestDTO request) {
         return quick_service.sort(request);
+    }
+
+    @PostMapping("/heap_sort")
+    public List<Step> heap_sort(@RequestBody RequestDTO request) {
+        return heap_service.sort(request);
+    }
+
+    @PostMapping("/merge_sort")
+    public List<Step> merge_sort(@RequestBody RequestDTO request) {
+        return merge_service.sort(request);
     }
 }
